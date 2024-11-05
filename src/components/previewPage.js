d33df2860049b38646199cde5eba5ddff59d68d5
JISOO LEE <67191178+bazzang@users.noreply.github.com>
2024-11-05 23:18:36 +0900

    modified:   src/components/previewPage.js 	modified:   src/components/productionHistoryPage.js

--- src/components/previewPage.js
+++ src/components/previewPage.js
@@ -7,40 +7,40 @@ function PreviewPage() {
 
   return (
     <>
-        <div class="watermark">
+        <div className="watermark">
             <p>구매 후 워터마크를 제거해주세요.
             <br/>
             <br/>2024.10.02 23:59까지 사용 가능합니다.
             <br/>
             <br/>워터마크를 제거하셔도 무제한 수정이 가능합니다.</p>
-            <div class="btn-wrap">
-                <a href="#" class="watermark-buy">구매하러 가기</a>
-                <a href="#" class="watermark-remove">워터마크 제거하기</a>
+            <div className="btn-wrap">
+                <a href="#" className="watermark-buy">구매하러 가기</a>
+                <a href="#" className="watermark-remove">워터마크 제거하기</a>
             </div>
         </div>
-        <div class="wedding-card">
-            <section class="main"> 
-                <div class="cardbg-img">
-                    <img src="./images/card/typo.png" class="main-typo"/>
-                    <img src="./images/card/page3_bg.png" class="marry-main-bg"/> 
+        <div className="wedding-card">
+            <section className="main"> 
+                <div className="cardbg-img">
+                    <img src="./images/card/typo.png" className="main-typo"/>
+                    <img src="./images/card/page3_bg.png" className="marry-main-bg"/> 
                 </div>
-                <div class="info-card">
-                    <div class="title-name">
+                <div className="info-card">
+                    <div className="title-name">
                         <p>김견우</p>
                         <p>그리고</p>
                         <p>임직녀</p>
                     </div>
-                    <div class="name-txt">
-                        <strong>김견우</strong><span class="cursive">and</span><strong> 임직녀</strong>
-                        <div class="m-days">
-                            <div class="wedding-info">
-                                <span class="marry-year">2024년</span>
-                                <span class="marry-month">10월</span>
-                                <span class="marry-date">31일</span>
-                                <span class="marry-day">목요일</span>
-                                <span class="marry-hour">오후 1시</span>
+                    <div className="name-txt">
+                        <strong>김견우</strong><span className="cursive">and</span><strong> 임직녀</strong>
+                        <div className="m-days">
+                            <div className="wedding-info">
+                                <span className="marry-year">2024년</span>
+                                <span className="marry-month">10월</span>
+                                <span className="marry-date">31일</span>
+                                <span className="marry-day">목요일</span>
+                                <span className="marry-hour">오후 1시</span>
                             </div>
-                            <p class="marry-location">아가테아트 1층 팬톤홀</p>
+                            <p className="marry-location">아가테아트 1층 팬톤홀</p>
                         </div>
                     </div>
                 </div>
--- src/components/productionHistoryPage.js
+++ src/components/productionHistoryPage.js
@@ -7,79 +7,79 @@ function ProductionHistoryPage() {
     return (
         <>
         
-            <div class="contents-wrap">
-                <div class="container">
-                    <div class="using">
-                        <strong class="using-title">이용기간 안내</strong>
-                        <ul class="using-list">
+            <div className="contents-wrap">
+                <div className="container">
+                    <div className="using">
+                        <strong className="using-title">이용기간 안내</strong>
+                        <ul className="using-list">
                             <li>예식 후 <strong>30일까지</strong> 이용 가능합니다.</li>
                             <li>예식일 <strong> 335일(11개월) 전부터</strong> 제작이 가능합니다.</li>
                             <li>워터마크 미제거 청첩장은 <strong>7일</strong>이 지나면 자동으로 삭제됩니다.</li>
                         </ul>
                     </div>
-                    <div class="list-btn"><button class="btn-create">제작하기</button></div>
-                    <strong class="list-title">모바일 청첩장</strong>
-                    <div class="wd-list"> 
-                        <div class="wd-item">
-                            <div class="wd-thumb"><img src="./images/list/wd_thumb.png" alt=""/></div>
-                            <div class="wd-contents">
-                                <div class="wd-info">
+                    <div className="list-btn"><button className="btn-create">제작하기</button></div>
+                    <strong className="list-title">모바일 청첩장</strong>
+                    <div className="wd-list"> 
+                        <div className="wd-item">
+                            <div className="wd-thumb"><img src="./images/list/wd_thumb.png" alt=""/></div>
+                            <div className="wd-contents">
+                                <div className="wd-info">
                                     <div>
-                                        <strong class="wd-title">홍길동님의 청첩장-1</strong>
-                                        <ul class="wd-desc">
+                                        <strong className="wd-title">홍길동님의 청첩장-1</strong>
+                                        <ul className="wd-desc">
                                             <li>2024. 09. 01 21:30 제작</li> 
                                             <li>워터마크 제거여부 : <strong>제거안함</strong></li> 
-                                            <li>사용기간 : <span class="red">2024. 10. 02. 23:59</span> 까지</li> 
+                                            <li>사용기간 : <span className="red">2024. 10. 02. 23:59</span> 까지</li> 
                                         </ul>
                                     </div>
-                                    <div class="wd-btn">
-                                        <button class="wd-btn-gray">복제</button>
-                                        <button class="wd-btn-blue">수정</button>
-                                        <button class="wd-btn-red">삭제</button>
+                                    <div className="wd-btn">
+                                        <button className="wd-btn-gray">복제</button>
+                                        <button className="wd-btn-blue">수정</button>
+                                        <button className="wd-btn-red">삭제</button>
                                     </div>
                                 </div>
-                                <div class="wd-etc">
-                                    <div class="wd-option">
-                                        <button class="wd-option-btn"><img src="./images/list/wd_option_icon_1.svg" alt=""/>청첩장 확인하기</button>
-                                        <button class="wd-option-btn"><img src="./images/list/wd_option_icon_2.svg" alt=""/>참석여부 확인</button>
-                                        <button class="wd-option-btn"><img src="./images/list/wd_option_icon_3.svg" alt=""/>URL 복사하기</button>
-                                        <button class="wd-option-btn"><img src="./images/list/wd_option_icon_4.svg" alt=""/>카톡 공유하기</button>
-                                        <button class="wd-option-btn"><img src="./images/list/wd_option_icon_5.svg" alt=""/>QR코드</button>
+                                <div className="wd-etc">
+                                    <div className="wd-option">
+                                        <button className="wd-option-btn"><img src="./images/list/wd_option_icon_1.svg" alt=""/>청첩장 확인하기</button>
+                                        <button className="wd-option-btn"><img src="./images/list/wd_option_icon_2.svg" alt=""/>참석여부 확인</button>
+                                        <button className="wd-option-btn"><img src="./images/list/wd_option_icon_3.svg" alt=""/>URL 복사하기</button>
+                                        <button className="wd-option-btn"><img src="./images/list/wd_option_icon_4.svg" alt=""/>카톡 공유하기</button>
+                                        <button className="wd-option-btn"><img src="./images/list/wd_option_icon_5.svg" alt=""/>QR코드</button>
                                     </div>
-                                    <div class="wd-btn">
-                                        <button class="wd-btn-gray">워터마크 제거하기</button>
+                                    <div className="wd-btn">
+                                        <button className="wd-btn-gray">워터마크 제거하기</button>
                                     </div>  
                                 </div>
                             </div> 
                         </div>
-                        <div class="wd-item">
-                            <div class="wd-thumb"><img src="./images/wd_thumb.png" alt=""/></div>
-                            <div class="wd-contents">
-                                <div class="wd-info">
+                        <div className="wd-item">
+                            <div className="wd-thumb"><img src="./images/wd_thumb.png" alt=""/></div>
+                            <div className="wd-contents">
+                                <div className="wd-info">
                                     <div>
-                                        <strong class="wd-title">홍길동님의 청첩장-2</strong>
-                                        <ul class="wd-desc">
+                                        <strong className="wd-title">홍길동님의 청첩장-2</strong>
+                                        <ul className="wd-desc">
                                             <li>2024. 09. 01 21:30 제작</li> 
                                             <li>워터마크 제거여부 : <strong>제거안함</strong></li> 
-                                            <li>사용기간 : <span class="red">2024. 10. 02. 23:59</span> 까지</li> 
+                                            <li>사용기간 : <span className="red">2024. 10. 02. 23:59</span> 까지</li> 
                                         </ul>
                                     </div>
-                                    <div class="wd-btn">
-                                        <button class="wd-btn-gray">복제</button>
-                                        <button class="wd-btn-blue">수정</button>
-                                        <button class="wd-btn-red">삭제</button>
+                                    <div className="wd-btn">
+                                        <button className="wd-btn-gray">복제</button>
+                                        <button className="wd-btn-blue">수정</button>
+                                        <button className="wd-btn-red">삭제</button>
                                     </div>
                                 </div>
-                                <div class="wd-etc">
-                                    <div class="wd-option">
-                                        <button class="wd-option-btn"><img src="./images/wd_option_icon_1.svg" alt=""/>청첩장 확인하기</button>
-                                        <button class="wd-option-btn"><img src="./images/wd_option_icon_2.svg" alt=""/>참석여부 확인</button>
-                                        <button class="wd-option-btn"><img src="./images/wd_option_icon_3.svg" alt=""/>URL 복사하기</button>
-                                        <button class="wd-option-btn"><img src="./images/wd_option_icon_4.svg" alt=""/>카톡 공유하기</button>
-                                        <button class="wd-option-btn"><img src="./images/wd_option_icon_5.svg" alt=""/>QR코드</button>
+                                <div className="wd-etc">
+                                    <div className="wd-option">
+                                        <button className="wd-option-btn"><img src="./images/wd_option_icon_1.svg" alt=""/>청첩장 확인하기</button>
+                                        <button className="wd-option-btn"><img src="./images/wd_option_icon_2.svg" alt=""/>참석여부 확인</button>
+                                        <button className="wd-option-btn"><img src="./images/wd_option_icon_3.svg" alt=""/>URL 복사하기</button>
+                                        <button className="wd-option-btn"><img src="./images/wd_option_icon_4.svg" alt=""/>카톡 공유하기</button>
+                                        <button className="wd-option-btn"><img src="./images/wd_option_icon_5.svg" alt=""/>QR코드</button>
                                     </div>
-                                    <div class="wd-btn">
-                                        <button class="wd-btn-gray">워터마크 제거하기</button>
+                                    <div className="wd-btn">
+                                        <button className="wd-btn-gray">워터마크 제거하기</button>
                                     </div>  
                                 </div>
                             </div> 
@@ -88,19 +88,19 @@ function ProductionHistoryPage() {
                 </div>
             </div>
 
-            <div class="footer-wrap">
-                <div class="container">
+            <div className="footer-wrap">
+                <div className="container">
                     <footer>
-                        <div class="ft-top">
-                            <div class="ft-top-left">
-                                <img class="ft-logo" src="./images/footer_logo.png" alt=""/>
-                                <div class="ft-link">
+                        <div className="ft-top">
+                            <div className="ft-top-left">
+                                <img className="ft-logo" src="./images/footer_logo.png" alt=""/>
+                                <div className="ft-link">
                                     <a href="#">개인정보처리방침</a>
                                     <a href="#">이용약관</a>
                                 </div>
                             </div>
-                            <div class="ft-top-right">
-                                <ul class="ft-info">
+                            <div className="ft-top-right">
+                                <ul className="ft-info">
                                     <li>상호명. 유포리아</li>  
                                     <li>대표. 이현진</li> 
                                     <li>사업자 등록번호. 184-46-01168</li> 
@@ -108,19 +108,19 @@ function ProductionHistoryPage() {
                                     <li>전화. 010-6669-6430 이메일: wlsl8013@naver.com</li> 
                                     <li>주소. 서울특별시 강남구 테헤란로82길 15, 19호(대치동, 디아이타워)</li>
                                 </ul>
-                                <p class="copyright">COPYRIGHT NeedIT. All rights reserved.</p>
+                                <p className="copyright">COPYRIGHT NeedIT. All rights reserved.</p>
                             </div> 
                         </div>
-                        <div class="ft-bottom">
-                            <div class="sitemap">
+                        <div className="ft-bottom">
+                            <div className="sitemap">
                                 <a href="#">주문하기</a>
                                 <a href="#">자주묻는질문</a>
                                 <a href="#">고객센터</a>
                                 <a href="#">모바일 청첩장</a>
                             </div>
-                            <div class="site-name">Site name</div>
-                            <div class="site-desc">I'm a paragraph. Click here to add your own text and edit me.<br/>Hosting by I'MWEB</div>
-                            <div class="site-link">
+                            <div className="site-name">Site name</div>
+                            <div className="site-desc">I'm a paragraph. Click here to add your own text and edit me.<br/>Hosting by I'MWEB</div>
+                            <div className="site-link">
                                 <a href="#">이용약관</a> 
                                 <a href="#">개인정보처리방침</a> 
                             </div>
