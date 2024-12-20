67829f27bff03c0eb671b22a7d915ea806a010bb
JISOO LEE <67191178+bazzang@users.noreply.github.com>
2024-12-20 14:02:21 +0900

    modified:   src/components/InvitationProvider.js

--- src/components/InvitationProvider.js
+++ src/components/InvitationProvider.js
@@ -122,7 +122,7 @@ export const InvitationProvider = ({ children }) => {
     
     // 화환 보내기
     sendWreath: false, // 화환 보내기 섹션 활성화 여부
-    sendWreathUrl : "https://www.99flower.co.kr/", // 화환 보내기에 연결 될 url 
+    sendWreathUrl : "", // 화환 보내기에 연결 될 url 
     
     // 함께한 시간
     firstMeetTime: null, // 첫 만남 날짜 및 시간
