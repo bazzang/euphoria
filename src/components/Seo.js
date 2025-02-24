42c09c379ea3464907f5502ec8932a52541de6fb
JISOO LEE <67191178+bazzang@users.noreply.github.com>
2025-02-25 00:33:17 +0900

    modified:   public/index.html 	modified:   src/components/Seo.js

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -4,13 +4,13 @@ function SEO({ title, description, image }) {
   return (
     <Helmet>
       <title>{title}</title>
-      <meta name="description" content={description} />
+      <meta name="description" content={description || "소중한 순간을 유포리아와 함께하세요."} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
-      <meta property="og:image" content={image} />
+      <meta property="og:title" content={title || "Euphoria"} />
+      <meta property="og:image" content={image || "https://euphoria-psi.vercel.app/wd_thumb.png"} />
       <meta property="og:image:width" content="260" />
       <meta property="og:image:height" content="260" />
-      <meta property="og:description" content={description} />
+      <meta property="og:description" content={description || "소중한 순간을 함께하세요."}  />
       <meta property="og:locale" content="ko_KR" />
     </Helmet>
   );
